27a14cc1325fa8432a63d58f3ba2fd62feb4b1e7
CloudPepper Developer <renbranmadelo@gmail.com>
2025-08-03 13:21:09 +0400

    Fix React Router basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/thuraya">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
